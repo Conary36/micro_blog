097ae864262fd5d0f8248770e6a4447f18a19fe0
Conary36 <crossingwaters@icloud.com>
2022-03-20 16:38:27 -0400

    ..saving..

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {}; // Key with value of an array of comments
+
+app.get("/posts/:id/comments", (req, res) => {});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || []; //If comments is undefined, set it to an empty array
+
+  comments.push({ id: commentId, content });//Push the comment into the array
+
+  commentsByPostId[req.params.id] = comments;// Set the commentsByPostId to the comments array
+
+  res.status(201).send(comments); // Send the comments array back to the client
+});
+
+app.listen(4001, () => {
+  console.log("Server is running on port 4001");
+});
